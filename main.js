182632517dbd295b282ddfefdeb9fa309cf19b17
HostX <51702129+HostXX@users.noreply.github.com>
2020-09-17 16:17:30 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ require('dotenv/config')
 
 const port = process.env.PORT || 5000;
 
-// impoting db conection instance
+// importing db conection instance
 
 require('./src/apis/database/databaseConection')
 
